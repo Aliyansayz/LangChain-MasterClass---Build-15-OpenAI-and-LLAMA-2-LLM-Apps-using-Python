34cf055811a5f50d7ba3c7a1aaf077c920db88b0
Aliyan Anwar <56589240+Aliyansayz@users.noreply.github.com>
2023-11-01 20:09:25 +0500

    Update utils.py

--- S18 - Project 9 - Support Chat Bot for your Website/Support Chat Bot For Your Website -  Project 9/utils.py
+++ S18 - Project 9 - Support Chat Bot for your Website/Support Chat Bot For Your Website -  Project 9/utils.py
@@ -4,6 +4,9 @@ from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddi
 import pinecone
 import asyncio
 from langchain.document_loaders.sitemap import SitemapLoader
+from langchain.document_loaders import DirectoryLoader
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
 
 
 #Function to fetch data from website
@@ -20,6 +23,18 @@ def get_website_data(sitemap_url):
 
     return docs
 
+
+def load_docs(directory):
+  loader = DirectoryLoader(directory)
+  documents = loader.load()
+  return documents
+
+
+# number of docs
+# documents = load_docs(directory)
+# len(documents)
+
+
 #Function to split data into smaller chunks
 def split_data(docs):
 
@@ -33,9 +48,13 @@ def split_data(docs):
     return docs_chunks
 
 #Function to create embeddings instance
-def create_embeddings():
+def create_embeddings(model_name="all-MiniLM-L6-v2"):
 
-    embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+    embeddings = SentenceTransformerEmbeddings(model_name)
+    return embeddings
+
+def create_embeddings_openai():
+    embeddings = OpenAIEmbeddings(model_name="ada")
     return embeddings
 
 #Function to push data to Pinecone
@@ -68,5 +87,17 @@ def get_similar_docs(index,query,k=2):
 
     similar_docs = index.similarity_search(query, k=k)
     return similar_docs
+
+
+
+
+
+def get_answer_from_openai(query,  similar_docs, model_name="gpt-3.5-turbo" ):
+  model_name = "gpt-3.5-turbo"
+  llm = OpenAI(model_name)  
+  similar_docs = get_similiar_docs(query)
+  chain = load_qa_chain(llm, chain_type="stuff")  
+  answer = chain.run(input_documents=similar_docs, question=query)
+  return answer
 
 
