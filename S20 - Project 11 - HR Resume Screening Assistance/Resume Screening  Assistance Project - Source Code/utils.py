66f8d61920439a4f199b53b20602ee135f40186d
Aliyan Anwar <56589240+Aliyansayz@users.noreply.github.com>
2023-11-03 19:02:37 +0500

    Update utils.py

--- S20 - Project 11 - HR Resume Screening Assistance/Resume Screening  Assistance Project - Source Code/utils.py
+++ S20 - Project 11 - HR Resume Screening Assistance/Resume Screening  Assistance Project - Source Code/utils.py
@@ -12,30 +12,23 @@ from langchain import HuggingFaceHub
 
 
 #Extract Information from PDF file
-def get_pdf_text(pdf_doc):
+def pdf_textual(filename) :
+
     text = ""
-    pdf_reader = PdfReader(pdf_doc)
-    for page in pdf_reader.pages:
+    pdf_ = PdfReader(filename)
+    for page in pdf_.pages:
         text += page.extract_text()
     return text
 
 
-
 # iterate over files in
 # that user uploaded PDF files, one by one
-def create_docs(user_pdf_list, unique_id):
-    docs=[]
-    for filename in user_pdf_list:
-        
-        chunks=get_pdf_text(filename)
+def form_document_stack(user_pdf_list, unique_id):
+  docs = []
+  for filename in user_pdf_list:
+      docs.append(Document( page_content= pdf_textual(filename), metadata={"name": f"{filename}" , "unique_id":unique_id } ) )
 
-        #Adding items to our list - Adding data & its metadata
-        docs.append(Document(
-            page_content=chunks,
-            metadata={"name": filename.name,"id":filename.id,"type=":filename.type,"size":filename.size,"unique_id":unique_id},
-        ))
-
-    return docs
+  return docs
 
 
 #Create embeddings instance
@@ -91,8 +84,8 @@ def similar_docs(query,k,pinecone_apikey,pinecone_environment,pinecone_index_nam
 # Helps us get the summary of a document
 def get_summary(current_doc):
     llm = OpenAI(temperature=0)
-    #llm = HuggingFaceHub(repo_id="bigscience/bloom", model_kwargs={"temperature":1e-10})
-    chain = load_summarize_chain(llm, chain_type="map_reduce")
+    llm = HuggingFaceHub(repo_id="bigscience/bloom", model_kwargs={"temperature":1e-10})
+    # chain = load_summarize_chain(llm, chain_type="map_reduce")
     summary = chain.run([current_doc])
 
     return summary
